f53066b2ffbe3a63130a78c3fce76b45c5fd8307
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-12 21:43:00 -0300

    Implementando regra de negócio

--- src/br/com/treinaweb/javafx/agenda/repositorios/impl/ContatoRepositorio.java
+++ src/br/com/treinaweb/javafx/agenda/repositorios/impl/ContatoRepositorio.java
@@ -7,6 +7,39 @@
 
 package br.com.treinaweb.javafx.agenda.repositorios.impl;
 
-public class ContatoRepositorio {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.treinaweb.javafx.agenda.entidades.Contato;
+import br.com.treinaweb.javafx.agenda.repositorios.interfaces.AgendaRepositorio;
+
+public class ContatoRepositorio implements AgendaRepositorio<Contato> {
+
+	private static List<Contato> contatos = new ArrayList<Contato>();
+
+	@Override
+	public List<Contato> selecionar() {
+		return contatos;
+	}
+
+	@Override
+	public void inserir(Contato entidade) {
+		contatos.add(entidade);
+	}
+
+	@Override
+	public void atualizar(Contato entidade) {
+		var original = contatos.stream().filter(contato -> contato.getNome().equals(entidade.getNome())).findFirst();
+		if (original.isPresent()) {
+			original.get().setNome(entidade.getNome());
+			original.get().setIdade(entidade.getIdade());
+			original.get().setTelefone(entidade.getTelefone());
+		}
+	}
+
+	@Override
+	public void excluir(Contato entidade) {
+		contatos.remove(entidade);
+	}
 
 }
