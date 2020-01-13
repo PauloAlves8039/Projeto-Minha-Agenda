016aef87d6a72a568a0dfdf18058dde994ff8461
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-12 21:05:00 -0300

    Criando a interface AgendaRepositorio

---
+++ src/br/com/treinaweb/javafx/agenda/repositorios/interfaces/AgendaRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.treinaweb.javafx.agenda.repositorios.interfaces;
+
+import java.util.List;
+
+public interface AgendaRepositorio<T> {
+	
+	List<T> selecionar();
+	void inserir(T entidade);
+	void atualizar(T entidade);
+	void excluir(T entidade);
+}
