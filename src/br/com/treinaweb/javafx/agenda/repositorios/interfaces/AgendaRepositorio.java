3eb9d0861f13c70439118af21a331f3c9f5d715b
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-12 21:14:53 -0300

    Adicionando descrição

--- src/br/com/treinaweb/javafx/agenda/repositorios/interfaces/AgendaRepositorio.java
+++ src/br/com/treinaweb/javafx/agenda/repositorios/interfaces/AgendaRepositorio.java
@@ -1,3 +1,10 @@
+/*
+ * Arquivo: interface - AgendaRepositorio
+ * Autor: Paulo Alves
+ * Descrição: responsável por métodos genéricos referentes a regra de negócio da aplicação
+ * Data: 12/01/2020
+*/
+
 package br.com.treinaweb.javafx.agenda.repositorios.interfaces;
 
 import java.util.List;
