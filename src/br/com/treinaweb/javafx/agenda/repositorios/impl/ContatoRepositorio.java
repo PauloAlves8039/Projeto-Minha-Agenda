765131c585615952955ec5a7a939c124148e3f56
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-12 21:10:16 -0300

    Criando a classe ContatoRepositorio

---
+++ src/br/com/treinaweb/javafx/agenda/repositorios/impl/ContatoRepositorio.java
@@ -0,0 +1,12 @@
+/*
+ * Arquivo: Classe - ContatoRepositorio
+ * Autor: Paulo Alves
+ * Descrição: responsável por implementar métodos da interface AgendaRepositorio
+ * Data: 12/01/2020
+*/
+
+package br.com.treinaweb.javafx.agenda.repositorios.impl;
+
+public class ContatoRepositorio {
+
+}
