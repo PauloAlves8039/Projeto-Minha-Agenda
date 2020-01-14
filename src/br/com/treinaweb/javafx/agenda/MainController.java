d87fd2df1afd1e558534b8c92417e51217db06c0
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-14 19:47:14 -0300

    Definindo funcionalidades da aplicação

--- src/br/com/treinaweb/javafx/agenda/MainController.java
+++ src/br/com/treinaweb/javafx/agenda/MainController.java
@@ -7,6 +7,141 @@
 
 package br.com.treinaweb.javafx.agenda;
 
-public class MainController {
-	
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import br.com.treinaweb.javafx.agenda.entidades.Contato;
+import br.com.treinaweb.javafx.agenda.repositorios.impl.ContatoRepositorio;
+import br.com.treinaweb.javafx.agenda.repositorios.interfaces.AgendaRepositorio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private TableView<Contato> tabelaContatos;
+	@FXML
+	private Button botaoInserir;
+	@FXML
+	private Button botaoAlterar;
+	@FXML
+	private Button botaoExcluir;
+	@FXML
+	private TextField txfNome;
+	@FXML
+	private TextField txfIdade;
+	@FXML
+	private TextField txfTelefone;
+	@FXML
+	private Button botaoSalvar;
+	@FXML
+	private Button botaoCancelar;
+
+	private Boolean ehInserir;
+	private Contato contatoSelecionado;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		habilitarEdicaoAgenda(false);
+		this.tabelaContatos.getSelectionModel().selectedItemProperty()
+				.addListener((observador, contatoAntigo, contatoNovo) -> {
+					if (contatoNovo != null) {
+						txfNome.setText(contatoNovo.getNome());
+						txfIdade.setText(String.valueOf(contatoNovo.getIdade()));
+						txfTelefone.setText(contatoNovo.getTelefone());
+						this.contatoSelecionado = contatoNovo;
+					}
+				});
+		carregarTabelaContatos();
+	}
+
+	private void carregarTabelaContatos() {
+		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+		List<Contato> contatos = repositorioContato.selecionar();
+		if (contatos.isEmpty()) {
+			Contato contato = new Contato();
+			contato.setNome("Paulo Alves");
+			contato.setIdade(23);
+			contato.setTelefone("(81)98888-7777");
+			contatos.add(contato);
+		}
+		ObservableList<Contato> contatosObservableList = FXCollections.observableArrayList(contatos);
+		this.tabelaContatos.getItems().setAll(contatosObservableList);
+	}
+
+	public void botaoInserir_Action() {
+		this.ehInserir = true;
+		this.txfNome.setText("");
+		this.txfIdade.setText("");
+		this.txfTelefone.setText("");
+		habilitarEdicaoAgenda(true);
+	}
+
+	public void botaoExcluir_Action() {
+		Alert confirmacao = new Alert(AlertType.CONFIRMATION);
+		confirmacao.setTitle("Confirmação");
+		confirmacao.setHeaderText("Confirmação da exclusão do contato");
+		confirmacao.setContentText("Tem certeza de que deseja excluir este contato?");
+		Optional<ButtonType> resultadoConfirmacao = confirmacao.showAndWait();
+		if (resultadoConfirmacao.isPresent() && resultadoConfirmacao.get() == ButtonType.OK) {
+			AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+			repositorioContato.excluir(this.contatoSelecionado);
+			carregarTabelaContatos();
+			this.tabelaContatos.getSelectionModel().selectFirst();
+		}
+	}
+
+	public void botaoCancelar_Action() {
+		habilitarEdicaoAgenda(false);
+		this.tabelaContatos.getSelectionModel().selectFirst();
+	}
+
+	public void botaoSalvar_Action() {
+		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+		Contato contato = new Contato();
+		contato.setNome(txfNome.getText());
+		contato.setIdade(Integer.parseInt(txfIdade.getText()));
+		contato.setTelefone(txfTelefone.getText());
+		if (this.ehInserir) {
+			repositorioContato.inserir(contato);
+		} else {
+			repositorioContato.atualizar(contato);
+		}
+		habilitarEdicaoAgenda(false);
+		carregarTabelaContatos();
+		this.tabelaContatos.getSelectionModel().selectFirst();
+	}
+
+	public void botaoAlterar_Action() {
+		habilitarEdicaoAgenda(true);
+		this.ehInserir = false;
+		this.txfNome.setText(this.contatoSelecionado.getNome());
+		this.txfIdade.setText(Integer.toString(this.contatoSelecionado.getIdade()));
+		this.txfTelefone.setText(this.contatoSelecionado.getTelefone());
+	}
+
+	private void habilitarEdicaoAgenda(Boolean edicaoEstaHabilitada) {
+		this.txfNome.setDisable(!edicaoEstaHabilitada);
+		this.txfIdade.setDisable(!edicaoEstaHabilitada);
+		this.txfTelefone.setDisable(!edicaoEstaHabilitada);
+		this.botaoSalvar.setDisable(!edicaoEstaHabilitada);
+		this.botaoCancelar.setDisable(!edicaoEstaHabilitada);
+		this.botaoInserir.setDisable(edicaoEstaHabilitada);
+		this.botaoAlterar.setDisable(edicaoEstaHabilitada);
+		this.botaoExcluir.setDisable(edicaoEstaHabilitada);
+		this.tabelaContatos.setDisable(edicaoEstaHabilitada);
+	}
+
 }
